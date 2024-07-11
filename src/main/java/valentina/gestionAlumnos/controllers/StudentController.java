5091ff6d52fe13268015d72399561dad6a91dc1a
ValentinaOrlas <johanavop25@gmail.com>
2024-07-10 22:40:06 -0500

    Add method POST

--- src/main/java/valentina/gestionAlumnos/controllers/StudentController.java
+++ src/main/java/valentina/gestionAlumnos/controllers/StudentController.java
@@ -1,8 +1,9 @@
 package valentina.gestionAlumnos.controllers;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import valentina.gestionAlumnos.domain.Student;
 
 import java.util.ArrayList;
@@ -21,4 +22,37 @@ public class StudentController {
     ));
 
 
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Student> getStudents (){
+        return students;
+    }
+
+
+    @RequestMapping(value = "/{email}", method = RequestMethod.GET)
+    public Student getStudentEmail ( @PathVariable String email){
+        for(Student i : students){
+            if(i.getEmail().equalsIgnoreCase(email)){
+                 return i;
+            }
+        }
+        return null;
+    }
+
+
+    private String messageExistStudent(){
+        return "it already exists";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> createStudent(@RequestBody Student student){
+
+        for(Student i : students){
+            if(i.getId() == student.getId() || i.getEmail().equalsIgnoreCase(student.getEmail())) {
+                return new ResponseEntity<>(messageExistStudent(), HttpStatus.CONFLICT);
+            }
+
+        }
+        students.add(student);
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
+    }
 }
